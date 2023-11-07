b2acdb5ea92dad9fcf3388b149d9af6f364b4de2
Oliver Wasner <oli.wasner@hotmail.com>
2023-11-07 16:42:29 +0100

    ViewModel & Account Pages

---
+++ project-3-quiz/ViewModels/JwtTokenResponseVM.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace project_3_quiz.ViewModels
+{
+    public class JwtTokenResponseVM
+    {
+        [JsonPropertyName("AccessToken")]
+        public string AccessToken { get; set; }
+    }
+}
