97cf4a70b11f69751db9636a63d825503e832f2a
Sparven704 <arvid.lju@gmail.com>
2023-11-22 16:07:52 +0200

    Fix Register, Login, CreateQuestion, CreateQuiz, Quiz, QuizList

--- project-3-quiz/ViewModels/OptionVM.cs
+++ project-3-quiz/ViewModels/OptionVM.cs
@@ -3,6 +3,8 @@
     public class OptionVM
     {
         public string Text { get; set; }
+        public string SingleOption { get; set; }
         public bool IsChecked { get; set; }
+        public bool IsCorrect { get; set; }
     }
 }
