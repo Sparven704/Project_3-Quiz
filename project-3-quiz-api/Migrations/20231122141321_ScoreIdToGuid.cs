ec96cbd2edb725362de8c1b503816910fc3bc9fe
Oliver Wasner <oli.wasner@hotmail.com>
2023-11-22 15:36:04 +0100

    ScoreId migration & FetchQuizEnpoint

---
+++ project-3-quiz-api/Migrations/20231122141321_ScoreIdToGuid.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace project_3_quiz_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class ScoreIdToGuid : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Scores",
+                table: "Scores");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Scores");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Id",
+                table: "Scores",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: Guid.NewGuid());
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Scores",
+                table: "Scores",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "Id",
+                table: "Scores");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Scores");
+
+            migrationBuilder.AddColumn<int>(
+                 name: "Id",
+                table: "Scores",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Scores",
+                table: "Scores",
+                column: "Id");
+        }
+    }
+}
